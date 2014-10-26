ca5f32f8db9974c82e7e2713d401f360cd3656c3
DoraH <DoraH@users.noreply.github.com>
2014-10-26 19:38:30 +0800

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -4,8 +4,8 @@ require(plyr)
 setwd('/Users/user/Desktop/Coursera/UCI HAR Dataset/')
 
 # Read data into data frames
-features        <- read.table('./features.txt', header=FALSE);
-activity_labels <- read.table('./activity_labels.txt', header=FALSE); 
+features        <- read.table('./features.txt', header=FALSE, colClasses = c("character"));
+activity_labels <- read.table('./activity_labels.txt', header=FALSE, col.names = c("ActivityId", "Activity")); 
 subject_train   <- read.table('./train/subject_train.txt', header=FALSE);
 subject_test    <- read.table('./test/subject_test.txt', header=FALSE);
 X_train         <- read.table('./train/X_train.txt', header=FALSE);
@@ -13,38 +13,45 @@ X_test          <- read.table('./test/X_test.txt', header=FALSE);
 y_train         <- read.table('./train/y_train.txt', header=FALSE);
 y_test          <- read.table('./test/y_test.txt', header=FALSE);
 
-## 3. Uses descriptive activity names to name the activities in the data set
-colnames(activity_labels) <- c("activityid", "activity")
-colnames(subject_train)   <- "subjectid"
-colnames(subject_test)    <- "subjectid"
-colnames(y_train)         <- "activity"
-colnames(y_test)          <- "activity"
-colnames(X_train)         <- features$V2
-colnames(X_test)          <- features$V2
-
-## 1. Merges the training and the test sets to create one data set
+## 1. Merges the training and test sets to create one data set
 
 # Combine files into one data set 
-trainingData <- cbind(subject_train, y_train, X_train)
-testData     <- cbind(subject_test, y_test, X_test)
+trainingData <- cbind(cbind(X_train, subject_train), y_train)
+testData     <- cbind(cbind(X_test, subject_test), y_test)
 combineData  <- rbind(trainingData, testData)
+
+# Label columns
+combineLabels      <- rbind(rbind(features, c(562, "Subject")), c(563, "ActivityId"))[,2]
+names(combineData) <- combineLabels
 
 ## 2. Extracts only the measurements on the mean and standard deviation for each measurement
 
-# Determine the columns containing "mean()" or "std()"
-meanstdcol <- grepl("mean\\(\\)", colnames(combineData)) | grepl("std\\(\\)", colnames(combineData))
-# Keep the subjectID and activity columns
-meanstdcol[1:2] <- TRUE
-# Remove unnecessary columns
-combineData <- combineData[, meanstdcol]
+meanstdcol <- combineData[,grepl("mean|std|Subject|ActivityId", names(combineData))]
+
+## 3. Uses descriptive activity names to name the activities in the data set
+
+meanstdcol <- merge(meanstdcol, activity_labels, by = "ActivityId", all.x=TRUE)
+meanstdcol <- meanstdcol[,-1]
 
 ## 4. Appropriately labels the data set with descriptive variable names
-# Remove parentheses, dash, commas
-cleancolnames = gsub("\\(|\\)|-|,", "", colnames(combineData))
-colnames(combineData) <- tolower(cleancolnames)
+
+# Remove parentheses, dash, commas and make clearer names
+names(meanstdcol) <- gsub('\\(|\\)',"",names(meanstdcol), perl = TRUE)
+names(meanstdcol) <- make.names(names(meanstdcol))
+names(meanstdcol) <- gsub('Acc',"Acceleration",names(meanstdcol))
+names(meanstdcol) <- gsub('GyroJerk',"GyroJerk",names(meanstdcol))
+names(meanstdcol) <- gsub('Gyro',"Gyro",names(meanstdcol))
+names(meanstdcol) <- gsub('Mag',"Magnitude",names(meanstdcol))
+names(meanstdcol) <- gsub('^t',"TimeDomain.",names(meanstdcol))
+names(meanstdcol) <- gsub('^f',"FrequencyDomain.",names(meanstdcol))
+names(meanstdcol) <- gsub('\\.mean',".Mean",names(meanstdcol))
+names(meanstdcol) <- gsub('\\.std',".StdDev",names(meanstdcol))
+names(meanstdcol) <- gsub('Freq\\.',"Frequency.",names(meanstdcol))
+names(meanstdcol) <- gsub('Freq$',"Frequency",names(meanstdcol))
+names(meanstdcol) <- gsub("([Bb]ody[Bb]ody|[Bb]ody)","Body",names(meanstdcol))
 
 ## 5. creates a second, independent tidy data set with the average of each variable for each activity and each subject
 # Summarize data
-tidyData = ddply(combineData, .(subjectid, activity), numcolwise(mean))
+tidyData <- ddply(meanstdcol, c("Subject","Activity"), numcolwise(mean))
 # Export tidyData set
-write.table(tidyData, file="tidyData.txt", sep = "\t", row.names = FALSE)
+write.table(tidyData, file = "tidyData.txt", sep = "\t", row.names = FALSE)
